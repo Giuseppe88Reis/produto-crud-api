594c7e48f77fe0a9421f0d9cdc5d95e4541d22e7
Giuseppe88Reis <giuseppereis88@gmail.com>
2022-03-26 09:25:57 -0300

    Primeira parte(instalação de dependências)

---
+++ script-sql/script-produto.sql
@@ -0,0 +1,36 @@
+-- Database: Produto
+
+-- DROP DATABASE IF EXISTS "Produto";
+
+CREATE DATABASE "Produto"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "Produto"
+    IS 'Tabelas a serem criadas:
+Produto;
+Cliente;';
+
+Create Table produto (
+	id_produto SERIAL PRIMARY KEY NOT NULL,
+	nome VARCHAR(40) NOT NULL,
+	preco FLOAT NOT NULL,
+	quant_est INTEGER NOT NULL,
+	descricao VARCHAR(100)
+)
+
+Create Table cliente(
+	id_cliente SERIAL PRIMARY KEY NOT NULL,
+	nome VARCHAR(40) NOT NULL,
+	cnpj VARCHAR(20) NOT NULL,
+	endereco VARCHAR(80), 
+	//email/USER
+	//senha
+)
+
+select * from produto
