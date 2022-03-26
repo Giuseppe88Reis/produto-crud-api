ad74abf4a8221173c7170f6993007a971884e11f
Giuseppe88Reis <giuseppereis88@gmail.com>
2022-03-26 18:48:56 -0300

    Desenvolvimento do CRUD

--- produto-api/src/controllers/produto.controller.js
+++ produto-api/src/controllers/produto.controller.js
@@ -15,3 +15,33 @@ exports.createProduto = async(req,res) => {
         },
     });
 };
+
+//Método para listar os produtos
+exports.listAllProduto = async(req, res) => {
+    const response = await db.query('SELECT * FROM produto ORDER BY id_produto ASC');
+    res.status(200).send(response.rows);
+};
+
+//Método para listar os produtos por Id
+exports.findProdutoById = async(req, res) => {
+    const produtoId = req.params.id;
+    const response = await db.query('SELECT * FROM produto WHERE id_produto = $1', [produtoId]);
+    res.status(200).send(response.rows);
+};
+
+//Método para alterar os produtos por Id
+exports.updateProdutoById = async(req, res) =>{
+    const produtoId = req.params.id;
+    const {nome, preco, quant_est, descricao} = req.body;
+    const response = await db.query('UPDATE produto SET nome = $1, preco = $2, quant_est = $3, descricao = $4 WHERE id_produto = $5',  [nome, preco, quant_est, descricao, produtoId]
+    );
+    res.status(200).send({message: 'Alteração do produto realizada com sucesso!'});
+};
+
+//Método para excluir os produtos por Id
+exports.deleteProdutoById = async(req, res)=>{
+    const produtoId = req.params.id;
+    await db.query('DELETE FROM produto WHERE id_produto = $1', [produtoId]);
+
+    res.status(200).send({message: 'Produto excluído com sucesso!'});
+};
--- produto-api/src/routes/produto.routes.js
+++ produto-api/src/routes/produto.routes.js
@@ -4,5 +4,14 @@ const produtoController = require('../controllers/produto.controller');
 
 //Rota responsável por criar um produto
 router.post('/produto',produtoController.createProduto);
+//Listar todos os produtos
+router.get('/produto', produtoController.listAllProduto);
+//Listar por Id
+router.get('/produto/:id', produtoController.findProdutoById);
+//Alterar por Id
+router.put('/produto/:id', produtoController.updateProdutoById);
+//Excluir por Id
+router.delete('/produto/:id', produtoController.deleteProdutoById);
+
 
 module.exports = router;
