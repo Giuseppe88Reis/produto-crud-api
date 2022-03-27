b70e729a9ef95fe9662c7296d1a37b45ee4c40bc
Giuseppe88Reis <giuseppereis88@gmail.com>
2022-03-26 22:46:00 -0300

    Criação das rotas Angular

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProdutoAddComponent } from './produto-add/produto-add.component';
+import { ProdutoEditComponent } from './produto-edit/produto-edit.component'; 
+import { ProdutoGetComponent } from './produto-get/produto-get.component'; 
+
+const routes: Routes = [
+  {path:'produto/create', component: ProdutoAddComponent },
+  {path:'produto/:id', component: ProdutoEditComponent },
+  {path:'produto', component: ProdutoGetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
